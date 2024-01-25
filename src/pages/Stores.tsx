0c9a0ac841c7b91d465efe88de87da8dbe0188c4
salvatoluice <salvatoluice5@gmail.com>
2024-01-25 10:12:00 +0300

    launch

--- src/pages/Stores.tsx
+++ src/pages/Stores.tsx
@@ -46,6 +46,32 @@ const Stores: React.FC = () => {
                     Get Extra 5% Bonus at over 1000 stores
                 </p>
             </div>
+            <div className="grid grid-cols-2 md:grid-cols-6 w-full lg:grid-cols-7 gap-4 my-[2%]">
+              <div className="w-full h-[120px] flex justify-center items-end text-white font-medium capitalize text-[20px] rounded-md bg-[url('https://images.unsplash.com/photo-1647221598270-e7a8716fef08?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OTB8fGRpc2NvdW50fGVufDB8fDB8fHww')]">
+                fashion & clothing
+              </div>
+              <div className="w-full h-[120px] flex justify-center items-end text-white font-medium capitalize text-[20px] rounded-md bg-[url('https://images.unsplash.com/photo-1551288049-bebda4e38f71?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8ODR8fGRpc2NvdW50fGVufDB8fDB8fHww')]">
+                Beauty
+              </div>
+              <div className="w-full h-[120px] flex justify-center items-end text-white font-medium capitalize text-[20px] rounded-md bg-[url('https://images.unsplash.com/photo-1423666523292-b458da343f6a?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NzJ8fGRpc2NvdW50fGVufDB8fDB8fHww')]">
+                Cleaning
+              </div>
+              <div className="w-full h-[120px] flex justify-center items-end text-white font-medium capitalize text-[20px] rounded-md bg-[url('https://images.unsplash.com/photo-1545535408-2b4d520cbd88?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NDd8fGRpc2NvdW50fGVufDB8fDB8fHww')]">
+                photography
+              </div>
+              <div className="w-full h-[120px] flex justify-center items-end text-white font-medium capitalize text-[20px] rounded-md bg-[url('https://images.unsplash.com/photo-1566997560041-002fd549180b?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mzl8fGRpc2NvdW50fGVufDB8fDB8fHww')]">
+                videography
+              </div>
+              <div className="w-full h-[120px] flex justify-center items-end text-white font-medium capitalize text-[20px] rounded-md bg-[url('https://images.unsplash.com/photo-1607082350899-7e105aa886ae?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mjd8fGRpc2NvdW50fGVufDB8fDB8fHww')]">
+                Tech
+              </div>
+              <div className="w-full h-[120px] flex justify-center items-end text-white font-medium capitalize text-[20px] rounded-md bg-[url('https://images.unsplash.com/photo-1607082348824-0a96f2a4b9da?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTZ8fGRpc2NvdW50fGVufDB8fDB8fHww')]">
+                photo mount
+              </div>
+            </div>
+            <p className="font-medium text-[18px] md:text-[25px]">
+              Double Cash Back Event Featured Stores | Jan 2024
+            </p>
             <div className="grid grid-cols-2 md:grid-cols-6 w-full lg:grid-cols-6 gap-4">
                 {storeData.map((store) => (
                 <Link to={`/stores/${store.id}/view`} key={store.id} className="bg-white flex flex-col items-center justify-center rounded-md p-4 shadow-md hover:shadow-xl cursor-pointer">
