ef1b98f44ca4d16c326c2980ce4fcbd21cfb6af6
salvatoluice <salvatoluice5@gmail.com>
2024-03-11 10:13:19 +0300

    more on contact

--- src/index/Contact.tsx
+++ src/index/Contact.tsx
@@ -7,17 +7,19 @@ const ContactUsPage: React.FC = () => {
     <>
       <Navbar />
       <div className="px-[5%] mx-auto bg-gray-100 py-8">
-        <h1 className="text-3xl font-bold mb-4">Contact Us</h1>
+        <h1 className="text-3xl font-bold mb-4">Get in Touch</h1>
 
         <div className="bg-white shadow-md p-8 rounded-lg">
           <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
             <div>
-              <h2 className="text-xl font-bold mb-4">Our Location</h2>
-              <p className="mb-2">Britam Towers, Upperhill Nairobi</p>
+              <h2 className="text-xl font-bold mb-4">Visit Us</h2>
+              <p className="mb-2">Experience our services in person:</p>
+              <p className="mb-2">Britam Towers, Upperhill, Nairobi</p>
               <p className="mb-2">Nairobi, Kenya</p>
             </div>
             <div>
               <h2 className="text-xl font-bold mb-4">Contact Information</h2>
+              <p className="mb-2">Reach out to us for any inquiries:</p>
               <p className="mb-2">
                 Email: <a href="mailto:info@dthree.com">info@dthree.com</a>
               </p>
@@ -28,33 +30,33 @@ const ContactUsPage: React.FC = () => {
           <div className="mt-8">
             <h2 className="text-xl font-bold mb-4">Send Us a Message</h2>
             <form className="flex flex-col gap-4">
-              <div className="flex w-full flex-col gap-4 md:flex-row">
-                <div className='w-full'>
-                <label htmlFor="name" className="block mb-2 font-semibold">
-                  Your Name
-                </label>
-                <input
-                  type="text"
-                  id="name"
-                  name="name"
-                  placeholder="Enter your name"
-                  className="w-full border rounded-md outline-none px-4 py-2"
-                />
+              <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+                <div>
+                  <label htmlFor="name" className="block mb-2 font-semibold">
+                    Your Name
+                  </label>
+                  <input
+                    type="text"
+                    id="name"
+                    name="name"
+                    placeholder="Enter your name"
+                    className="w-full border rounded-md outline-none px-4 py-2"
+                  />
+                </div>
+                <div>
+                  <label htmlFor="email" className="block mb-2 font-semibold">
+                    Your Email
+                  </label>
+                  <input
+                    type="email"
+                    id="email"
+                    name="email"
+                    placeholder="Enter your email"
+                    className="w-full border rounded-md outline-none px-4 py-2"
+                  />
+                </div>
               </div>
-              <div className='w-full'>
-                <label htmlFor="email" className="block mb-2 font-semibold">
-                  Your Email
-                </label>
-                <input
-                  type="email"
-                  id="email"
-                  name="email"
-                  placeholder="Enter your email"
-                  className="w-full border rounded-md outline-none px-4 py-2"
-                />
-              </div>
-              </div>
-              <div className="col-span-2">
+              <div>
                 <label htmlFor="message" className="block mb-2 font-semibold">
                   Your Message
                 </label>
@@ -66,7 +68,7 @@ const ContactUsPage: React.FC = () => {
                   className="w-full border rounded-md outline-none px-4 py-2"
                 ></textarea>
               </div>
-              <div className="col-span-2">
+              <div>
                 <button
                   type="submit"
                   className="bg-[#ed4e50] text-white px-6 py-2 rounded-md hover:bg-blue-600 transition-colors"
