029b5662231405970e3a1a83c21b3a2dc3f3b2ef
salvatoluice <salvatoluice5@gmail.com>
2024-06-13 15:36:25 +0300

    redeployment test

---
+++ src/components/Owner/OwnerAppointments.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import SellerLayout from '../../elements/SellerLayout'
+
+const OwnerAppointments = () => {
+  return (
+    <SellerLayout>
+        <div className=""></div>
+    </SellerLayout>
+  )
+}
+
+export default OwnerAppointments
