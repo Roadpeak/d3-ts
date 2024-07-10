e6595c0b4e63cb5d10e0f4ad7e478591972ee79e
salvatoluice <salvatoluice5@gmail.com>
2024-07-10 16:15:04 +0300

    make appointment

--- src/pages/Deals.tsx
+++ src/pages/Deals.tsx
@@ -6,6 +6,7 @@ import { Link } from 'react-router-dom';
 import axios from 'axios';
 import CategorySlider from '../utils/elements/CategorySlider';
 import AllDeals from './AllDeals';
+import { useAuth } from '../utils/context/AuthContext';
 
 interface Store {
   _id: string;
@@ -35,6 +36,7 @@ interface Discount {
 const Deals: React.FC = () => {
   const [discounts, setDiscounts] = useState<Discount[]>([]);
   const [loading, setLoading] = useState<boolean>(true);
+  const { user } = useAuth();
 
   useEffect(() => {
     const fetchDiscountsByShop = async () => {
@@ -54,6 +56,13 @@ const Deals: React.FC = () => {
   return (
     <div>
       <Navbar />
+      {user && user.first_discount === 0 && (
+        <div className="bg-yellow-200">
+          <p className="text-yellow-800 px-4 text-center py-3 text-sm">
+            You have one free voucher. You will use it to access any discount and book an appointment.
+          </p>
+        </div>
+      )}
       <div className="bg-gray-100 flex flex-col px-[5%] py-4">
         <p className="text-center"></p>
         <CategorySlider />
