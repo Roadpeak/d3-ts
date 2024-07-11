00c8b9caf6545d8db06266c68bdbec107fa6ed24
salvatoluice <salvatoluice5@gmail.com>
2024-07-11 09:20:53 +0300

    media query

--- src/components/chat/ChatPage.tsx
+++ src/components/chat/ChatPage.tsx
@@ -3,9 +3,10 @@ import axios from 'axios';
 import Pusher from 'pusher-js';
 import { FiUpload } from 'react-icons/fi';
 import moment from 'moment';
-import { FaRegUserCircle, FaUser } from 'react-icons/fa';
-import { IoFilterSharp, IoHomeOutline } from 'react-icons/io5';
+import { FaChevronLeft, FaRegUserCircle, FaUser } from 'react-icons/fa';
+import { IoFilterSharp, IoHomeOutline, IoSendOutline } from 'react-icons/io5';
 import { IoMdMenu } from 'react-icons/io';
+import { MdAttachFile } from 'react-icons/md';
 
 interface Message {
     id: number;
@@ -35,6 +36,17 @@ const ChatPage: React.FC = () => {
     const [newMessage, setNewMessage] = useState<string>('');
     const [attachment, setAttachment] = useState<File | null>(null);
     const messageEndRef = useRef<HTMLDivElement | null>(null);
+
+    const [isMobileView, setIsMobileView] = useState<boolean>(window.innerWidth <= 768);
+
+    useEffect(() => {
+        const handleResize = () => {
+            setIsMobileView(window.innerWidth <= 768);
+        };
+
+        window.addEventListener('resize', handleResize);
+        return () => window.removeEventListener('resize', handleResize);
+    }, []);
 
     useEffect(() => {
         const pusher = new Pusher('f566a73b20b1548b5a13', {
@@ -115,7 +127,6 @@ const ChatPage: React.FC = () => {
         }
     };
 
-
     const fetchMessages = async (userId: number) => {
         try {
             const response = await axios.get(`https://api.discoun3ree.com/api/messages/${userId}`, {
@@ -168,102 +179,114 @@ const ChatPage: React.FC = () => {
     return (
         <div className="flex w-full h-screen md:p-8 bg-gray-50">
             <div className="flex w-full border rounded">
-                <div className="w-1/3 bg-gray-100 border border-gray-200 py-4 overflow-y-auto">
-                    <div className="flex mb-1 items-center w-full justify-between">
-                        <a href='/' className="px-4">Chat</a>
-                        <div className="flex items-center gap-2 mr-4 text-gray-600">
-                            <a href="/" className="">
-                                <IoHomeOutline />
-                            </a>
-                            <a href="/accounts/profile" className="">
-                                <FaRegUserCircle />
-                            </a>
-                            <IoMdMenu />
+                {!isMobileView || (isMobileView && selectedConversation === null) ? (
+                    <div className={`w-1/3 bg-gray-100 border border-gray-200 py-4 overflow-y-auto ${isMobileView ? 'w-full' : 'w-1/3'}`}>
+                        <div className="flex mb-1 items-center w-full justify-between">
+                            <a href='/' className="px-4">Chat</a>
+                            <div className="flex items-center gap-2 mr-4 text-gray-600">
+                                <a href="/" className="">
+                                    <IoHomeOutline />
+                                </a>
+                                <a href="/accounts/profile" className="">
+                                    <FaRegUserCircle />
+                                </a>
+                                <IoMdMenu />
+                            </div>
                         </div>
-                    </div>
-                    <div className="flex px-4 text-gray-600 w-full justify-between items-center gap-1 mb-4">
-                        <input className='w-[90%] bg-transparent border py-1 px-4 border-gray-200 rounded-full outline-none focus:bg-white text-[15px]' type="text" placeholder='Search...'/>
-                        <IoFilterSharp className='cursor-pointer' size={24} />
-                    </div>
-                    {conversations.map((conversation) => (
-                        <div
-                            key={conversation.id}
-                            onClick={() => handleConversationClick(conversation.id)}
-                            className={`cursor-pointer hover:bg-gray-50 h-fit border-b border-gray-400 px-4 py-3 ${selectedConversation === conversation.id ? 'bg-white' : ''}`}
-                        >
-                            <div className="flex justify-between items-center">
-                                <div className="flex items-center gap-1">
-                                    <FaUser className="w-10 h-10 rounded-full inline-block mr-2 text-gray-300 border border-gray-300" />                            
-                                <div>
-                                    <div>{conversation.first_name} {conversation.last_name}</div>
-                                    <div className="text-gray-600 text-sm">{conversation.last_message}</div>
-                                </div>
-                                </div>
-                                <div className="text-gray-500 text-xs">
-                                    {conversation.last_message_time && moment(conversation.last_message_time).format('h:mm A')}
-                                </div>
-                            </div>  
-                            {conversation.unseenCount > 0 && (
-                                <span className="ml-2 text-sm text-red-500">
-                                    {conversation.unseenCount} new
-                                </span>
-                            )}
+                        <div className="flex px-4 text-gray-600 w-full justify-between items-center gap-1 mb-4">
+                            <input className='w-[90%] bg-transparent border py-1 px-4 border-gray-200 rounded-full outline-none focus:bg-white text-[15px]' type="text" placeholder='Search...'/>
+                            <IoFilterSharp className='cursor-pointer' size={24} />
                         </div>
-                    ))}
-
-                </div>
-                <div className="flex-1 p-4 relative bg-white">
-                    <div className="flex flex-col bg-white overflow-y-auto h-[90%] w-full">
-                        {messages.map((message) => (
+                        {conversations.map((conversation) => (
                             <div
-                                key={message.id}
-                                className={`mb-4 ${message.from_id === selectedConversation ? 'self-start' : 'self-end'}`}
+                                key={conversation.id}
+                                onClick={() => handleConversationClick(conversation.id)}
+                                className={`cursor-pointer hover:bg-gray-50 h-fit border-b border-gray-400 px-4 py-3 ${selectedConversation === conversation.id ? 'bg-white' : ''}`}
                             >
-                                <div
-                                    className={`px-4 py-1.5 w-full rounded-lg ${message.from_id === selectedConversation ? 'bg-red-100' : 'bg-gray-200'}`}
-                                >
-                                    <p>{message.body}</p>
-                                    {message.attachment && (
-                                        <img
-                                            src={`https://api.discoun3ree.com/storage/${message.attachment}`}
-                                            alt="attachment"
-                                            className="max-w-full max-h-40 my-2"
-                                        />
-                                    )}
-                                    <div className="text-xs text-gray-500 text-end">
-                                        {moment(message.created_at).format('h:mm A')}
+                                <div className="flex justify-between items-center">
+                                    <div className="flex items-center gap-1">
+                                        <FaUser className="w-10 h-10 rounded-full inline-block mr-2 text-gray-300 border border-gray-300" />                            
+                                    <div>
+                                        <div>{conversation.first_name} {conversation.last_name}</div>
+                                        <div className="text-gray-600 text-sm">{conversation.last_message}</div>
                                     </div>
-                                </div>
+                                    </div>
+                                    <div className="text-gray-500 text-xs">
+                                        {conversation.last_message_time && moment(conversation.last_message_time).format('h:mm A')}
+                                    </div>
+                                </div>  
+                                {conversation.unseenCount > 0 && (
+                                    <span className="ml-2 text-sm text-red-500">
+                                        {conversation.unseenCount} new
+                                    </span>
+                                )}
                             </div>
                         ))}
-                        <div ref={messageEndRef} />
                     </div>
-                    {selectedConversation && (
-                        <div className="absolute bottom-0 left-0 right-0 p-4 h-[10%] bg-white flex items-center">
+                ) : null}
+                {selectedConversation !== null && (
+                    <div className={`flex-1 p-4 flex flex-col h-full bg-white ${isMobileView ? 'w-full' : 'w-2/3'}`}>
+                        <div className="flex gap-4 items-center mb-4 bg-white fixed top-0 left-0 right-0 p-4 z-10 shadow-md">
+                            {isMobileView && (
+                                <button
+                                    className="text-primary"
+                                    onClick={() => setSelectedConversation(null)}
+                                >
+                                    <FaChevronLeft />
+                                </button>
+                            )}
+                            <div>
+                                {isMobileView && selectedConversation && (
+                                    <div>
+                                        <h2 className="text-[16px] font-medium text-gray-600">
+                                            {conversations.find(c => c.id === selectedConversation)?.first_name} {conversations.find(c => c.id === selectedConversation)?.last_name}
+                                        </h2>
+                                    </div>
+                                )}
+                            </div>
+                        </div>
+                        <div className="flex-1 overflow-y-auto mt-14 mb-4">
+                            {messages.map((message) => (
+                                <div key={message.id} className={`mb-2 ${message.from_id === selectedConversation ? 'text-left' : 'text-right'}`}>
+                                    <div className={`inline-block p-2 rounded-lg ${message.from_id === selectedConversation ? 'bg-gray-200' : 'bg-primary text-white'}`}>
+                                        <p className="text-sm">{message.body}</p>
+                                        {message.attachment && (
+                                            <div>
+                                                <img src={message.attachment} alt="attachment" className="w-40 h-40 object-cover mt-2 rounded-lg" />
+                                            </div>
+                                        )}
+                                        <span className={`text-xs ${message.from_id !== selectedConversation ? 'text-gray-50' : ''} text-end block`}>{moment(message.created_at).format('h:mm A')}</span>
+                                    </div>
+                                </div>
+                            ))}
+                            <div ref={messageEndRef} />
+                        </div>
+                        <div className="mt-4 flex items-center">
                             <input
                                 type="text"
                                 value={newMessage}
                                 onChange={(e) => setNewMessage(e.target.value)}
-                                className="flex-1 p-2 border border-gray-300 rounded mr-2"
-                                placeholder="Type your message..."
+                                className="flex-1 border border-gray-300 rounded-full py-2 px-4 mr-2"
+                                placeholder="Type your message"
                             />
-                            <label className="cursor-pointer">
-                                <FiUpload className="w-6 h-6 mr-2 text-gray-600" />
-                                <input
-                                    type="file"
-                                    onChange={handleAttachmentChange}
-                                    className="hidden"
-                                />
+                            <input
+                                type="file"
+                                onChange={handleAttachmentChange}
+                                className="hidden"
+                                id="file-input"
+                            />
+                            <label htmlFor="file-input">
+                                <MdAttachFile className="cursor-pointer text-gray-500 mr-2" size={24} />
                             </label>
                             <button
                                 onClick={sendMessage}
-                                className="bg-primary text-white px-6 py-1.5 rounded-full"
+                                className="bg-primary text-white rounded-full p-2"
                             >
-                                Send
+                                <IoSendOutline size={24} />
                             </button>
                         </div>
-                    )}
-                </div>
+                    </div>
+                )}
             </div>
         </div>
     );
