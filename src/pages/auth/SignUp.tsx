713011ad56c8631dd53df3ac83be56bd1073d5a6
salvatoluice <salvatoluice5@gmail.com>
2024-08-04 17:53:44 +0300

    update

--- src/pages/auth/SignUp.tsx
+++ src/pages/auth/SignUp.tsx
@@ -4,7 +4,6 @@ import { Link, useNavigate } from 'react-router-dom';
 import { ClipLoader } from 'react-spinners';
 import logo from '../../assets/icon.png';
 import { FiEye, FiEyeOff } from 'react-icons/fi';
-import GoogleSignInButton from './GoogleSignInButton';
 
 const SignUp: React.FC = () => {
   const [formData, setFormData] = useState({
