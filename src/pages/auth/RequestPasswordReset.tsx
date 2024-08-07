f3e0030dec6ccbcdaf6725675fbdd1220091c4d7
salvatoluice <salvatoluice5@gmail.com>
2024-08-07 21:53:24 +0300

    password reset styles

--- src/pages/auth/RequestPasswordReset.tsx
+++ src/pages/auth/RequestPasswordReset.tsx
@@ -30,43 +30,43 @@ const RequestPasswordReset: React.FC = () => {
 
   return (
     <>
-    <div className="flex flex border bg-gray-100 items-center justify-center min-h-screen">
-      <div className="w-fit bg-white h-fit rounded-md flex flex-col md:flex-row">
-        <div className="bg-white p-8 rounded-lg w-full md:w-1/2 ">
-          <div className="text-center mb-2">
-            <a href="/">
-              <img src={logo} className='w-[50px] -mb-4 mx-auto' alt="Logo" />
-            </a>
-            <h1 className="text-[17px] font-medium text-black">Password reset</h1>
+      <div className="flex border bg-gray-100 items-center justify-center min-h-screen">
+        <div className="w-fit bg-white h-fit rounded-md flex flex-col md:flex-row">
+          <div className="bg-white p-8 rounded-lg w-full md:w-1/2 ">
+            <div className="text-center mb-2">
+              <a href="/">
+                <img src={logo} className='w-[50px] -mb-4 mx-auto' alt="Logo" />
+              </a>
+              <h1 className="text-[17px] font-medium text-black">Password reset</h1>
+            </div>
+            {message && <div className="text-green-500 text-[14px] mb-4">{message}</div>}
+            {error && <div className="text-red-500 text-[14px] mb-4">{error}</div>}
+            <form onSubmit={handleSubmit}>
+              <div className="mb-4">
+                <label className="text-[14px] text-black">Email Address</label>
+                <input
+                  type="email"
+                  className="p-2 block w-full text-[13px] font-light text-primary border-b border-gray-300 focus:border-primary focus:outline-none"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                  required
+                />
+              </div>
+              <button
+                type="submit"
+                className="w-full bg-primary font-medium text-[14px] text-white py-1.5 px-4 rounded-full disabled:opacity-50"
+                disabled={loading}
+              >
+                {loading ? 'Sending...' : 'Send Reset Link'}
+              </button>
+              <p className="text-[14px] text-gray-700 text-end mt-2">Remembered your password? <Link to='/accounts/sign-in' className="text-primary">Sign in</Link></p>
+            </form>
           </div>
-          {message && <div className="text-green-500 text-[14px] mb-4">{message}</div>}
-        {error && <div className="text-red-500 text-[14px] mb-4">{error}</div>}
-        <form onSubmit={handleSubmit}>
-          <div className="mb-4">
-            <label className="block text-gray-700">Email Address</label>
-            <input
-              type="email"
-              className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-primary"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-              required
-            />
+          <div className="hidden md:flex justify-center items-center ">
+            <img src="https://imgs.search.brave.com/VikpyiN7OTH_xj6mfR6zYxy8_mHlGuCGveLv7wIAg14/rs:fit:500:0:0/g:ce/aHR0cHM6Ly90My5m/dGNkbi5uZXQvanBn/LzA0LzUzLzMyLzc2/LzM2MF9GXzQ1MzMy/NzYyMF9mbExTaFJD/VU50cW9WTUszTnlm/SmRLSTFVblEzRHhC/eS5qcGc" alt="Illustration" />
           </div>
-          <button
-            type="submit"
-            className="w-full bg-primary text-white py-2 px-4 rounded-lg disabled:opacity-50"
-            disabled={loading}
-          >
-            {loading ? 'Sending...' : 'Send Reset Link'}
-          </button>
-          <p className="text-[14px] text-gray-700 text-end mt-2">Remembered your password? <Link to='/accounts/sign-in' className="text-primary">Sign in</Link></p>
-        </form>
-        </div>
-        <div className="hidden md:flex justify-center items-center ">
-          <img src="https://imgs.search.brave.com/VikpyiN7OTH_xj6mfR6zYxy8_mHlGuCGveLv7wIAg14/rs:fit:500:0:0/g:ce/aHR0cHM6Ly90My5m/dGNkbi5uZXQvanBn/LzA0LzUzLzMyLzc2/LzM2MF9GXzQ1MzMy/NzYyMF9mbExTaFJD/VU50cW9WTUszTnlm/SmRLSTFVblEzRHhC/eS5qcGc" alt="Illustration" />
         </div>
       </div>
-    </div>
     </>
   );
 };
--- src/pages/auth/ResetPassword.tsx
+++ src/pages/auth/ResetPassword.tsx
@@ -51,20 +51,20 @@ const ResetPassword: React.FC = () => {
         {success && <div className="text-green-500 mb-4">{success}</div>}
         <form onSubmit={handleResetPassword}>
           <div className="mb-4">
-            <label className="block text-gray-700">New Password</label>
+            <label className="text-[14px] text-black">New Password</label>
             <input
               type="password"
-              className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-primary"
+              className="p-2 block w-full text-[13px] font-light text-primary border-b border-gray-300 focus:border-primary focus:outline-none"
               value={password}
               onChange={(e) => setPassword(e.target.value)}
               required
             />
           </div>
           <div className="mb-4">
-            <label className="block text-gray-700">Confirm New Password</label>
+            <label className="text-[14px] text-black">Confirm New Password</label>
             <input
               type="password"
-              className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-primary"
+              className="p-2 block w-full text-[13px] font-light text-primary border-b border-gray-300 focus:border-primary focus:outline-none"
               value={confirmPassword}
               onChange={(e) => setConfirmPassword(e.target.value)}
               required
@@ -72,7 +72,7 @@ const ResetPassword: React.FC = () => {
           </div>
           <button
             type="submit"
-            className="w-full bg-primary text-white py-2 px-4 rounded-lg hover:bg-red-700 disabled:opacity-50"
+            className="w-full bg-primary font-medium text-[14px] text-white py-1.5 px-4 rounded-full disabled:opacity-50"
             disabled={loading}
           >
             {loading ? 'Resetting...' : 'Reset Password'}
